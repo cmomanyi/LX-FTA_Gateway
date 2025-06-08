b85d323adac0469e532d261ea3775a84773d7ac9
cmomanyi <ktondeh@gmail.com>
2025-06-08 18:51:04 -0500

    updated main.tf

--- infrastructure/terraform/frontend/main.tf
+++ infrastructure/terraform/frontend/main.tf
@@ -57,3 +57,32 @@ resource "aws_cloudfront_distribution" "frontend" {
     Name = "Frontend Distribution"
   }
 }
+
+resource "aws_iam_role_policy" "frontend_bucket_access" {
+  name = "FrontendBucketAccessPolicy"
+  role = "GitHubActionsDeployRole"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid      = "AllowListBucket",
+        Effect   = "Allow",
+        Action   = ["s3:ListBucket"],
+        Resource = "arn:aws:s3:::portal.lx-gateway.tech"
+      },
+      {
+        Sid      = "AllowObjectActions",
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:DeleteObject"
+        ],
+        Resource = "arn:aws:s3:::portal.lx-gateway.tech/*"
+      }
+    ]
+  })
+}
+
